234ea4feb2c5a517866d0597dc41b0da40c97ceb
gustavo L <gustalondon2016@gmail.com>
2021-05-28 17:03:24 -0500

    Mejorando el proyecto con router redux y pre procesador de css

--- src/components/HelloWorld.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import '../assets/styles/App.scss'
-const HelloWorld =() => (
-    <h1>Hola Mundo por fin</h1>
-)
-
-
-export default HelloWorld;
--- webpack.config.js
+++ webpack.config.js
@@ -38,10 +38,20 @@ module.exports = {
         'css-loader',
         'sass-loader',
       ],
+    },
+    {
+      test: /\.(png|gif|jpg)$/,
+      use: [
+        {
+          loader: 'file-loader',
+          options: { name: 'assets/[hash].[ext]' },
+        }
+      ],
     }
     ]
   },
   devServer: {
+    historyApiFallback: true,
     contentBase: './dist',
     port: 8081,
   },
